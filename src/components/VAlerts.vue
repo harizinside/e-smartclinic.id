88f6a3e384eabd304a518c75447c2cfd998be55e
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-08-19 17:33:23 +0700

    add default image with fix login

--- src/components/VAlerts.vue
+++ src/components/VAlerts.vue
@@ -1,28 +1,6 @@
 <template>
   <div>
     <div
-      class="flex p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400"
-      role="alert">
-      <svg
-        class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        viewBox="0 0 20 20">
-        <path
-          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-      </svg>
-      <span class="sr-only">Info</span>
-      <div>
-        <span class="font-medium">Ensure that these requirements are met:</span>
-        <ul class="mt-1.5 list-disc list-inside">
-          <li>At least 10 characters (and up to 100 characters)</li>
-          <li>At least one lowercase character</li>
-          <li>Inclusion of at least one special character, e.g., ! @ # ?</li>
-        </ul>
-      </div>
-    </div>
-    <div
       class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
       role="alert">
       <svg
@@ -36,11 +14,13 @@
       </svg>
       <span class="sr-only">Danger</span>
       <div>
-        <span class="font-medium">Ensure that these requirements are met:</span>
+        <span class="font-medium">{{ props.message }}</span>
         <ul class="mt-1.5 list-disc list-inside">
-          <li>At least 10 characters (and up to 100 characters)</li>
-          <li>At least one lowercase character</li>
-          <li>Inclusion of at least one special character, e.g., ! @ # ?</li>
+          <li
+            v-for="(err, index) in props.errors"
+            :key="index">
+            <span class="font-semibold">{{ err.field }}</span>&nbsp;{{ err.message }}
+          </li>
         </ul>
       </div>
     </div>
@@ -48,7 +28,15 @@
 </template>
 
 <script lang="ts" setup>
-
+const props = defineProps<{
+  type: string
+  message: string
+  errors: {
+    field: string
+    message: string
+    value: string
+  }[]
+}>()
 </script>
 
 <style></style>
