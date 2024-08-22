f6c1901d70a25408a4aded5fb55f0b7ee2caf4cb
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-08-22 11:31:40 +0700

    add better 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,38 @@
 <template>
-  <div class="404">
-    <h1>404</h1>
+  <div>
+    <AdminLayouts>
+      <div class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div class="text-center">
+          <p class="text-base font-semibold text-orange-600">
+            404
+          </p>
+          <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+            Page not found
+          </h1>
+          <p class="mt-6 text-base leading-7 text-gray-600">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div class="mt-10 flex items-center justify-center gap-x-6">
+            <route-link
+              to="/"
+              class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">
+              Go back home
+            </route-link>
+            <route-link
+              to="#"
+              class="text-sm font-semibold text-gray-900">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </route-link>
+          </div>
+        </div>
+      </div>
+    </AdminLayouts>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useHead } from '@unhead/vue'
+import AdminLayouts from '@view/AdminLayouts.vue'
 
 useHead({
   title: '404 | e-Smart Clinic'
