2db89d9b84af3188aaf0814c9325511e5396e44e
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-09-13 17:30:24 +0700

    update dependency with xhange date view to grid

--- src/views/practice-schedule/IndexView.vue
+++ src/views/practice-schedule/IndexView.vue
@@ -40,307 +40,36 @@
           </button>
         </div>
       </div>
-      <div class="overflow-x-auto pt-2">
-        <table
-          class="min-w-full">
-          <thead class="text-center border-t border-slate-300">
-            <tr>
-              <th
-                v-for="(row, index) in tableInfo.data?.slice(0, 7)"
-                :key="index"
-                scope="col"
-                class="py-3.5 pl-4 pr-3 text-sm font-semibold text-gray-900">
-                {{ $d(Date.parse(row.date), 'long') }} 
-              </th>
-            </tr>
-          </thead>
-          <tbody class="divide-y divide-gray-200 bg-white align-text-top">
-            <tr>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-t border-slate-300 pb-60">
-                <div class="flex flex-row gap-2 items-center mb-1">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-l border-t border-slate-300 pb-60">
-                <div class="flex flex-row gap-2 items-center">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-l border-t border-slate-300 pb-60">
-                <div class="flex flex-row gap-2 items-center">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-l border-t border-slate-300 pb-60">
-                <div class="flex flex-row gap-2 items-center">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-l border-t border-slate-300 pb-60">
-                <Popover class="relative">
-                  <PopoverButton
-                    as="div"
-                    class="flex flex-row gap-2 items-center cursor-pointer mb-1.5">
-                    <svg
-                      class="h-1.5 w-1.5 fill-red-500"
-                      viewBox="0 0 6 6"
-                      aria-hidden="true">
-                      <circle
-                        cx="3"
-                        cy="3"
-                        r="3" />
-                    </svg>
-                    <UserIcon
-                      class="h-3"
-                      aria-hidden="true" />
-                    <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                  </PopoverButton>
-                  <transition
-                    enter-active-class="transition duration-200 ease-out"
-                    enter-from-class="translate-y-1 opacity-0"
-                    enter-to-class="translate-y-0 opacity-100"
-                    leave-active-class="transition duration-150 ease-in"
-                    leave-from-class="translate-y-0 opacity-100"
-                    leave-to-class="translate-y-1 opacity-0">
-                    <PopoverPanel
-                      class="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-sm">
-                      <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5 bg-red-500 pt-1">
-                        <div class="bg-white p-7 lg:grid-cols-2">
-                          <div
-                            class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50">
-                            <div
-                              class="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                              <img
-                                class="w-10 h-10 rounded-full"
-                                src="https://avatar.iran.liara.run/public/girl?usearname=dr.Natalli+Mawardi"
-                                alt="dr. Damar Sajiwo, MARS">
-                            </div>
-                            <div class="">
-                              <p class="text-sm font-medium text-gray-900 mb-0.5">
-                                dr. Natalli Mawardi, dipl.CIBTAC
-                              </p>
-                              <p class="text-xs font-medium text-gray-400">
-                                446.1//3776/01-DPMPTSP/OL/2019
-                              </p>
-                            </div>
-                          </div>
-                          <div class="pt-4">
-                            <div class="flex flex-row items-center gap-2 mb-1.5">
-                              <MapPinIcon
-                                class="h-3"
-                                aria-hidden="true" />
-                              <span class="text-xs">NMW Klinik Petogogan</span>
-                            </div>
-                            <div class="flex flex-row items-center gap-2 mb-1.5">
-                              <ClockIcon
-                                class="h-3"
-                                aria-hidden="true" />
-                              <span class="text-xs">09:00 - 17:00 WIB</span>
-                            </div>
-                            <div class="flex flex-row items-center gap-2">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                class="bi bi-heart-pulse-fill h-3"
-                                viewBox="0 0 16 16">
-                                <path d="M1.475 9C2.702 10.84 4.779 12.871 8 15c3.221-2.129 5.298-4.16 6.525-6H12a.5.5 0 0 1-.464-.314l-1.457-3.642-1.598 5.593a.5.5 0 0 1-.945.049L5.889 6.568l-1.473 2.21A.5.5 0 0 1 4 9z" />
-                                <path d="M.88 8C-2.427 1.68 4.41-2 7.823 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C11.59-2 18.426 1.68 15.12 8h-2.783l-1.874-4.686a.5.5 0 0 0-.945.049L7.921 8.956 6.464 5.314a.5.5 0 0 0-.88-.091L3.732 8z" />
-                              </svg>
-                              <span class="text-xs">Konsultasi Umum (Rawat Jalan)</span>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="bg-gray-50 p-1.5 flex flex-row justify-between gap-2 text-sm">
-                          <div class="grid grid-cols-2">
-                            <button
-                              class="font-medium text-blue-600 dark:text-blue-500n p-2">
-                              Edit
-                            </button>
-                            <button
-                              class="font-medium text-red-600 dark:text-red-500 ms-3 p-2">
-                              Remove
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </PopoverPanel>
-                  </transition>
-                </Popover>
-                <Popover class="relative">
-                  <PopoverButton
-                    as="div"
-                    class="flex flex-row gap-2 items-center cursor-pointer">
-                    <svg
-                      class="h-1.5 w-1.5 fill-blue-500"
-                      viewBox="0 0 6 6"
-                      aria-hidden="true">
-                      <circle
-                        cx="3"
-                        cy="3"
-                        r="3" />
-                    </svg>
-                    <UserIcon
-                      class="h-3"
-                      aria-hidden="true" />
-                    <span>08.00  dr. Damar Sajiwo, MARS</span>
-                  </PopoverButton>
-                  <transition
-                    enter-active-class="transition duration-200 ease-out"
-                    enter-from-class="translate-y-1 opacity-0"
-                    enter-to-class="translate-y-0 opacity-100"
-                    leave-active-class="transition duration-150 ease-in"
-                    leave-from-class="translate-y-0 opacity-100"
-                    leave-to-class="translate-y-1 opacity-0">
-                    <PopoverPanel
-                      class="absolute left-1/2 z-10 mt-3 w-screen max-w-sm -translate-x-1/2 transform px-4 sm:px-0 lg:max-w-sm">
-                      <div class="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5 bg-blue-500 pt-1">
-                        <div class="bg-white p-7 lg:grid-cols-2">
-                          <div
-                            class="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50">
-                            <div
-                              class="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                              <img
-                                class="w-10 h-10 rounded-full"
-                                src="https://avatar.iran.liara.run/public/boy?usearname=dr.Damar+Sajiwo"
-                                alt="dr. Damar Sajiwo, MARS">
-                            </div>
-                            <div class="">
-                              <p class="text-sm font-medium text-gray-900 mb-0.5">
-                                dr. Damar Sajiwo, MARS
-                              </p>
-                              <p class="text-xs font-medium text-gray-400">
-                                446.1//3776/01-DPMPTSP/OL/2019
-                              </p>
-                            </div>
-                          </div>
-                          <div class="pt-4">
-                            <div class="flex flex-row items-center gap-2 mb-1.5">
-                              <MapPinIcon
-                                class="h-3"
-                                aria-hidden="true" />
-                              <span class="text-xs">NMW Klinik Petogogan</span>
-                            </div>
-                            <div class="flex flex-row items-center gap-2 mb-1.5">
-                              <ClockIcon
-                                class="h-3"
-                                aria-hidden="true" />
-                              <span class="text-xs">09:00 - 17:00 WIB</span>
-                            </div>
-                            <div class="flex flex-row items-center gap-2">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                class="bi bi-heart-pulse-fill h-3"
-                                viewBox="0 0 16 16">
-                                <path d="M1.475 9C2.702 10.84 4.779 12.871 8 15c3.221-2.129 5.298-4.16 6.525-6H12a.5.5 0 0 1-.464-.314l-1.457-3.642-1.598 5.593a.5.5 0 0 1-.945.049L5.889 6.568l-1.473 2.21A.5.5 0 0 1 4 9z" />
-                                <path d="M.88 8C-2.427 1.68 4.41-2 7.823 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C11.59-2 18.426 1.68 15.12 8h-2.783l-1.874-4.686a.5.5 0 0 0-.945.049L7.921 8.956 6.464 5.314a.5.5 0 0 0-.88-.091L3.732 8z" />
-                              </svg>
-                              <span class="text-xs">Konsultasi Umum (Rawat Jalan)</span>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="bg-gray-50 p-1.5 flex flex-row justify-between gap-2 text-sm">
-                          <div class="grid grid-cols-2">
-                            <button
-                              class="font-medium text-blue-600 dark:text-blue-500n p-2">
-                              Edit
-                            </button>
-                            <button
-                              class="font-medium text-red-600 dark:text-red-500 ms-3 p-2">
-                              Remove
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </PopoverPanel>
-                  </transition>
-                </Popover>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900 sm:pl-2 border-l border-t border-slate-300 pb-60">
-                <div class="flex flex-row gap-2 items-center">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-              <td class="whitespace-nowrap py-2 pl-4 pr-3 text-xs font-medium text-gray-900  bg-red-50 sm:pl-2 border-l border-t border-red-300 pb-60">
-                <div class="flex flex-row gap-2 items-center">
-                  <svg
-                    class="h-1.5 w-1.5 fill-red-500"
-                    viewBox="0 0 6 6"
-                    aria-hidden="true">
-                    <circle
-                      cx="3"
-                      cy="3"
-                      r="3" />
-                  </svg>
-                  <UserIcon
-                    class="h-3"
-                    aria-hidden="true" />
-                  <span>08.00  dr. Natalli Mawardi, dipl.CIBTAC</span>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="pt-2 w-full overflow-x-auto">
+        <div class="grid grid-cols-7 min-w-[1920px]">
+          <!-- <div class="flex flex-nowrap w-full overflow-x-auto"> -->
+          <div
+            v-for="(row, index) in tableInfo.data"
+            :key="index"
+            class="aspect-square w-full">
+            <div class="text-center border-t border-red-400">
+              title
+            </div>
+            <div
+              class="whitespace-normal py-2 pl-4 pr-3 text-xs font-medium text-gray-900 border-t border-r border-yellow-400 sm:pl-2 pb-60">
+              <div class="flex flex-row gap-2 items-center mb-1">
+                <svg
+                  class="h-1.5 w-1.5 fill-red-500"
+                  viewBox="0 0 6 6"
+                  aria-hidden="true">
+                  <circle
+                    cx="3"
+                    cy="3"
+                    r="3" />
+                </svg>
+                <UserIcon
+                  class="h-3"
+                  aria-hidden="true" />
+                <span class="">08.00  dr. Bambino Bismara Mamuaya, BMedSc(Hons)</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <TransitionRoot
         as="template"
@@ -356,7 +85,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { useHead } from '@unhead/vue'
 import { TransitionRoot } from '@headlessui/vue'
 import type { IPagination } from '@/interfaces/paginations'
@@ -392,6 +121,22 @@ const navs = ref<INavigation[]>([
 
 const dialogAdditional = ref<boolean>(false)
 const tableInfo = ref<IPagination<IData[]>>(JsonData)
+
+const panelClass = (index: number): string => {
+  if (index < 5) {
+    return 'border-r border-t'
+  } else if (index < 6) {
+    return 'border-t border-r'
+  } else if (index < 7) {
+    return 'border-t'
+  } else if (index < 12) {
+    return 'border-t border-b border-r'
+    } else if (index < 13) {
+    return 'border-t border-b border-r'
+  } else {
+    return 'border-t border-b'
+  }
+}
 
 useHead({
   title: 'Practice Schedule | e-Smart Clinic'
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import preload from 'vite-plugin-preload'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
@@ -10,6 +11,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    preload(),
     vueDevTools()
   ],
   resolve: {
