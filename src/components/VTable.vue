60e1d67fb2ce242f956850feff5be4981bbef6e5
Muhamad Haris Setiawan <muhamadharissetiawan@outlook.com>
2024-10-15 11:35:53 +0700

    change scroll to auto

--- src/components/VTable.vue
+++ src/components/VTable.vue
@@ -20,7 +20,7 @@
         @on-searching="searchTable" />
     </div>
   </div>
-  <div class="relative overflow-x-scroll sm:rounded-lg p-1 w-full">
+  <div class="relative overflow-x-auto sm:rounded-lg p-1 w-full">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <VTableHeader
